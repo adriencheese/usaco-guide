b123ce21c21fdc0f194d71bca1fbac71664df048
BuildTools <unconfigured@null.spigotmc.org>
2022-04-28 18:58:18 -0400

    Bovine final

--- TheBovineShuffle.cpp
+++ TheBovineShuffle.cpp
@@ -5,3 +5,58 @@
 using namespace std;
 
 
+const int MAX = 100;
+int shuffle[MAX];
+int cowid[MAX];
+
+
+int find(int n, int x) {
+    for (int i = 0; i < n; i++) {
+        // if element at index i == index, then return the index
+        if (shuffle[i] == x) {
+            return i;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    freopen("shuffle.in", "r", stdin);
+    freopen("shuffle.out", "w", stdout); 
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> shuffle[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> cowid[i];
+    }
+
+    //loop through 3 unshuffles
+    for (int i = 0; i < 3; i++) {
+        //temp arr for unshuffles
+        int temp[MAX];
+        for (int j = 1; j <= n;j++) {
+            //e is the index of where the cow in j index came from (what index it needs to go to in temp arr)
+            int e = find(n, j);
+
+            //populate the temp arr with the unswapped cowid's
+            temp[e] = cowid[j-1];
+        }
+        
+        //assign temp to cowid (temp is now the one-step unshuffled)
+        for (int j = 0; j < n; j++) {
+            cowid[j] = temp[j];
+        }
+
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << cowid[i] << '\n';
+    }
+}
+
