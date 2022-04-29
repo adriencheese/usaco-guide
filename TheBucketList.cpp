06f61433a89bb24474e42beb3da8caca93a3b25b
Adrien Qi <adrien.qi304@gmail.com>
2022-04-29 11:51:46 -0400

    initial bucketlist

--- TheBucketList.cpp
+++ TheBucketList.cpp
@@ -1,2 +1,15 @@
 //http://www.usaco.org/index.php?page=viewproblem2&cpid=856
 //https://usaco.guide/problems/usaco-856-the-bucket-list/solution
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+
+    
+    for(int j = 0; j < n; j++) {
+        int s, i, b; cin >> s >> i >> b;
+        
+    }
+}
